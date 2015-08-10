e6aa6c36e93e812c92e9760503db779a18004d9f
Davidzhu001 <qluoluo@icloud.com>
2015-08-10 11:04:35 -0500

    adjust the view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :articles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise  :database_authenticatable, 
+			:registerable,
+			:recoverable, 
+			:rememberable, 
+			:trackable, 
+			:validatable
 
 end
