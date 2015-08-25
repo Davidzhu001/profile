996f4ebc9c3a183889e2bd5a53d20e2126e4bf95
Davidzhu001 <qluoluo@icloud.com>
2015-08-24 20:53:52 -0500

    nested forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :articles
   has_many :skills, :dependent => :destroy
+   accepts_nested_attributes_for :skills, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 	devise  :database_authenticatable, 
