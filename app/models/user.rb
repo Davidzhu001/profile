f2a6861132d938e66177b0eb5ec2cd03fc01d2ba
Davidzhu001 <qluoluo@icloud.com>
2015-09-14 10:34:57 -0500

    make it private website

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ActiveRecord::Base
   has_many :skills, :dependent => :destroy
    accepts_nested_attributes_for :skills, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-	devise  :database_authenticatable, 
-			:registerable,
-			:recoverable, 
-			:rememberable, 
-			:trackable, 
+  # :confirmable, :lockable, :timeoutable and :omniauthable: registerable,
+	devise  :database_authenticatable,
+			:recoverable,
+			:rememberable,
+			:trackable,
 			:validatable
 
 end
