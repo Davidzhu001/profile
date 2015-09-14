3c121579424b22ed02d460ae6c75a3614ac8f54c
Davidzhu001 <qluoluo@icloud.com>
2015-09-14 10:35:56 -0500

    private

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@ class User < ActiveRecord::Base
   has_many :skills, :dependent => :destroy
    accepts_nested_attributes_for :skills, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
   # Include default devise modules. Others available are:
-<<<<<<< HEAD
   # :confirmable, :lockable, :timeoutable and :omniauthable
-=======
-  # :confirmable, :lockable, :timeoutable and :omniauthable: registerable,
->>>>>>> github
 	devise  :database_authenticatable,
 			:recoverable,
 			:rememberable,
